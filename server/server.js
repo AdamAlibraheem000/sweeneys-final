f978c4d77bfa8869670b5a8f04f9c4d424840b02
AdamAlibraheem000 <adamalibraheem@hotmail.com>
2022-07-04 20:35:00 -0400

    added path variable

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const bcrypt = require('bcrypt');
 const passport = require('passport');
 const flash = require('express-flash')
 const session = require('express-session')
+const path = require('path');
 
 
 require('dotenv').config();
